7b2e6fa3a5909ec3c43fafa891f2cf8b02f3a45c
Yuliia <example.com>
2023-10-11 18:55:25 +0300

    fixed MR comments

--- src/index.js
+++ src/index.js
@@ -1,30 +1,38 @@
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const healthCheckRouter = require('./routes/health-check');
+
+const options = {
+	failOnErrors: true,
+	definition: {
+		openapi: '3.0.0',
+		info: {
+			title: 'Express API for JSONPlaceholder',
+			version: '1.0.0',
+		},
+	},
+	apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
 
 const app = express();
 const PORT = 3000;
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-app.get('/health-check', (req, res) => {
-	res.json({ status: 'Server is running' });
-});
+app.use('/health-check', healthCheckRouter);
 
-app.use('/not-found', (req, res) => {
+app.use((_, res) => {
 	res.status(404).json({ error: 'Not Found' });
 });
 
-app.use('/internal-error', (req, res) => {
-	throw new Error('Simulated Internal Server Error');
-});
-
 app.use((err, req, res, next) => {
-	console.error(err.stack);
 	res.status(500).json({ error: 'Internal Server Error' });
 });
 
-// Start server on port 3000
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}`);
 });
